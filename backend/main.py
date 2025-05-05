281e0c1172bb72454354029de0a75c6768f7ffef
Shivam_srivastav-7838 <shivam.s@appgallop.com>
2025-05-05 13:22:52 +0530

    dynamic folder path

--- backend/main.py
+++ backend/main.py
@@ -5,13 +5,25 @@ import numpy as np
 import json
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import os
+
+base_dir = os.path.dirname(__file__)
+index_path = os.path.join(base_dir, "data", "free_faiss_index.index")
+index = faiss.read_index(index_path)
 
 # Load model and FAISS index
 model = SentenceTransformer('all-MiniLM-L6-v2')
-index = faiss.read_index("../data/free_faiss_index.index")
+# index = faiss.read_index("../data/free_faiss_index.index")
+index = faiss.read_index(index_path)
+
+# Get absolute path to the current file's directory
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct full path to the data file
+file_path = os.path.join(base_dir, "data", "free_ticket_data.json")
 
 # Load dataset
-with open("../data/free_ticket_data.json", "r", encoding="utf-8") as f:
+with open(file_path, "r", encoding="utf-8") as f:
     data = json.load(f)
 
 # Prepare texts and responses
